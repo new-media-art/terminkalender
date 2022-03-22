4b3767f0f5b32f8cb6a9af43732444b10483fe8d
Ulrich Fladl <ulricof@gmail.com>
2022-03-22 15:29:10 +0100

    2

--- src/data.js
+++ src/data.js
@@ -4,9 +4,69 @@ const calendarWeekData = [
         shortName: 'No',
         fullName: 'Now',
         events: [
-             { title: 'Vue.js learnen', edit: false, color: 'success' }
-             { title: 'Üben üben, üben', edit: false, color: 'success' }
-             { title: 'Vue.js learnen', edit: false, color: 'success' }
-        ]
-    }
+             { title: 'Vue.js learnen', edit: false, color: 'success' },
+             { title: 'Üben üben, üben', edit: false, color: 'success' },    
+        ],
+        active: false
+    },
+    {
+        id: 2,
+        shortName: 'No',
+        fullName: 'Now',
+        events: [
+             { title: 'Vue.js learnen', edit: false, color: 'success' },
+             { title: 'Üben üben, üben', edit: false, color: 'success' },    
+        ],
+        active: false
+    },
+    {
+        id: 3,
+        shortName: 'No',
+        fullName: 'Now',
+        events: [
+             { title: 'Vue.js learnen', edit: false, color: 'success' },
+             { title: 'Üben üben, üben', edit: false, color: 'success' },    
+        ],
+        active: false
+    },
+    {
+        id: 4,
+        shortName: 'No',
+        fullName: 'Now',
+        events: [
+             { title: 'Vue.js learnen', edit: false, color: 'success' },
+             { title: 'Üben üben, üben', edit: false, color: 'success' },    
+        ],
+        active: false
+    },
+    {
+        id: 5,
+        shortName: 'No',
+        fullName: 'Now',
+        events: [
+             { title: 'Vue.js learnen', edit: false, color: 'success' },
+             { title: 'Üben üben, üben', edit: false, color: 'success' },    
+        ],
+        active: false
+    },
+    {
+        id: 6,
+        shortName: 'No',
+        fullName: 'Now',
+        events: [
+             { title: 'Vue.js learnen', edit: false, color: 'success' },
+             { title: 'Üben üben, üben', edit: false, color: 'success' },    
+        ],
+        active: false
+    },
+    {
+        id: 7,
+        shortName: 'No',
+        fullName: 'Now',
+        events: [
+             { title: 'Vue.js learnen', edit: false, color: 'success' },
+             { title: 'Üben üben, üben', edit: false, color: 'success' },    
+        ],
+        active: false
+    },
 ]
