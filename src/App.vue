69d0c877dec8d4fafad6dd15a2e5dc2b3cef5747
Ulrich Fladl <ulricof@gmail.com>
2022-03-22 15:24:46 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,185 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="calendar-week">
+      <div class="container-fluid mt-5">
+        <div class="row">
+          <div class="col-12">
+            <div class="card-group">
+              <!--Tage-->
+              <div class="card">
+                <div class="card-header text-center">
+                  <strong>Montag</strong>
+                  </div>
+                  <div class="card-body">
+                    <div>1</div>
+                    <div class="alert alert-success text-center bg-vue">
+                      <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+              <div class="card">
+                <div class="card-header text-center">
+                  <strong>Dinstag</strong>
+                  </div>
+                  <div class="card-body">
+                    <div>2</div>
+                    <div class="alert alert-success text-center bg-vue">
+                      <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+              <div class="card">
+                <div class="card-header text-center">
+                  <strong>Mittwoch</strong>
+                  </div>
+                  <div class="card-body">
+                    <div>3</div>
+                    <div class="alert alert-success text-center bg-vue">
+                      <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+              <div class="card">
+                <div class="card-header text-center">
+                  <strong>Donnerstag</strong>
+                  </div>
+                  <div class="card-body">
+                    <div>4</div>
+                    <div class="alert alert-success text-center bg-vue">
+                      <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+              <div class="card">
+                <div class="card-header text-center">
+                  <strong>Freitag</strong>
+                  </div>
+                  <div class="card-body">
+                    <div>5</div>
+                    <div class="alert alert-success text-center bg-vue">
+                      <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+              <div class="card">
+                <div class="card-header text-center">
+                  <strong>Samstag</strong>
+                  </div>
+                  <div class="card-body">
+                    <div>6</div>
+                    <div class="alert alert-success text-center bg-vue">
+                      <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+              <div class="card">
+                <div class="card-header text-center">
+                  <strong>Sonntag</strong>
+                  </div>
+                  <div class="card-body">
+                    <div>7</div>
+                    <div class="alert alert-success text-center bg-vue">
+                      <div>Müll rausbringen</div>
+                    <div>
+                      <i class="fas fa-edit mr-2"></i>
+                      <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
+    <div id="calendar-entry">
+      <div class="container mt-5">
+      <div class="row">
+        <div class="col-6 offset-3">
+          <div class="card">
+            <div class="card-header text-center bg-vue">
+              <h5>Neuer Termin für: <strong>Dinstag</strong></h5>
+            </div>
+            <div class="card-body">
+              <input 
+              type="text"
+              class="form-control"
+              placeholder="Neuer Termin"
+              >
+              <div class="mt-3 text-center">
+                <span
+                  v-for="(color, index) in ['primary', 'success', 'info', 'warning', 'danger']"
+                  :key="index"
+                  class="alert mr-2 square"
+                  :class="['alert-' + color]"
+                  style="cursor: pointer"
+                ></span>
+              </div>
+              <hr>
+              <button class="btn bg-vue2 btn-block">Eintragen</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .bg-vue {
+    background-color: 52, 73, 94;
+    color: white;
+  }
+   .bg-vue2 {
+    background-color: 65, 184, 131;
+    
+  }
+  .square {
+    width: 40px;
+    height: 40px;
+  }
+  
 </style>
+
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const calendarWeekData = [
+    {
+        id: 1,
+        shortName: 'No',
+        fullName: 'Now',
+        events: [
+             { title: 'Vue.js learnen', edit: false, color: 'success' }
+             { title: 'Üben üben, üben', edit: false, color: 'success' }
+             { title: 'Vue.js learnen', edit: false, color: 'success' }
+        ]
+    }
+]
